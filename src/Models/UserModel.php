091dee9bd5f363028ae697b4fae4e0a02007c5c9
labitayoub <ayoub.labite@gmail.com>
2025-01-15 16:01:04 +0100

    separation des pages

--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -3,6 +3,7 @@ namespace App\Models;
 
 use App\Classes\Users;
 use App\Config\Database;
+use PDOException;
 use PDO;
 
 class UserModel{
@@ -36,5 +37,26 @@ class UserModel{
 
             }
     }
+
+    public function addMember($nom,$prenom, $email,$password,$role) {
+
+        $query = "INSERT INTO users (nom, prenom,email, password,role )
+                    VALUES (:nom, :prenom,:email, :password, :role );";
+        
+        $stmt = $this->connexion->prepare($query);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':role', $role);
+
+        try{
+
+        $stmt->execute();
+
+    } catch (PDOException $e) {
+        return $e->getMessage(); 
+    }
+}
     
 }
--- src/Views/index.php
+++ src/Views/index.php
@@ -11,9 +11,9 @@
     <div class="max-w-5xl mx-auto p-4 flex justify-between items-center">
     <a href="../../src/Views/index.php" class="text-2xl font-bold text-blue-600">Youdemy</a>
     <nav class="hidden md:flex gap-4">
-            <a href="../Views/" class="hover:text-blue-600">Home</a>
-            <a href="#about" class="hover:text-blue-600">About</a>
-            <a href="#courses" class="hover:text-blue-600">Courses</a>
+            <a href="../Views/" class="hover:text-blue-600 mt-2">Home</a>
+            <a href="#about" class="hover:text-blue-600 mt-2">About</a>
+            <a href="#courses" class="hover:text-blue-600 mt-2">Courses</a>
             <a href="../Views/auth/Login.php" class="border border-blue-600 text-blue-600 px-4 py-2 rounded">Login</a>
             <a href="../Views/auth/Register.php" class="bg-blue-600 text-white px-4 py-2 rounded">Sign Up</a>
         </nav>
